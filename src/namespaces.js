731db21236be95f04bc7d0ea222283d1511edd61
Cristian <cristianvasquez@gmail.com>
2022-11-22 14:54:56 +0100

    chore: use namespaces from lib

--- src/namespaces.js
+++ src/namespaces.js
@@ -1,20 +1,9 @@
-import namespace from '@rdfjs/namespace'
-import rdf from './rdf-ext.js'
+import {
+  schema, rdf, rdfs, skos, foaf, dash, xsd, sh, skosxl, vcard
+} from '@tpluscode/rdf-ns-builders'
 
 const ns = {
-  schema: rdf.namespace('http://schema.org/'),
-  vault: rdf.namespace('http://vault.org/'),
-  rdf: rdf.namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
-  rdfs: rdf.namespace('http://www.w3.org/2000/01/rdf-schema#'),
-  skos: rdf.namespace('http://www.w3.org/2004/02/skos/core#'),
-  local: rdf.namespace('http://localhost:8080/data/'),
-  foaf: rdf.namespace('http://xmlns.com/foaf/0.1/'),
-  ex: rdf.namespace('http://example.org/'),
-  purl: rdf.namespace('http://purl.org/dc/terms/'),
-  locn: rdf.namespace('http://www.w3.org/ns/locn#'),
-  xsd: rdf.namespace('http://www.w3.org/2001/XMLSchema#'),
-  sh: namespace('http://www.w3.org/ns/shacl#'),
-  dash: namespace('http://datashapes.org/dash#')
+  schema, rdf, rdfs, skos, foaf, dash, xsd, sh, skosxl, vcard
 }
 
 export { ns }
