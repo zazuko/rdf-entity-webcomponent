5b4d2f2f93a0410cf1a9f3ef4a2e6ebba4f440ae
Cristian <cristianvasquez@gmail.com>
2022-12-15 13:46:07 +0100

    feat: snapshots for html generation

---
+++ test/ResourceDescription.test.js
@@ -0,0 +1,24 @@
+import { expect } from 'expect'
+import toMatchSnapshot from 'expect-mocha-snapshot'
+import { describe, it } from 'mocha'
+import { ResourceDescription } from '../src/components/ResourceDescription.js'
+import { battery } from './battery.js'
+import { toClownface } from './support/serialization.js'
+import {
+  render as renderWebComponent
+} from '@lit-labs/ssr/lib/render-with-global-dom-shim.js'
+
+expect.extend({ toMatchSnapshot })
+
+describe('default', () => {
+  for (const [testName, [turtle, term]] of Object.entries(battery)) {
+    it(testName, function () {
+      const cf = toClownface(turtle, term)
+
+      const resourceWebComponent = ResourceDescription(cf, {})
+      const stringIterator = renderWebComponent(resourceWebComponent)
+      const html = Array.from(stringIterator).join('')
+      expect([turtle, term, html]).toMatchSnapshot(this)
+    })
+  }
+})
