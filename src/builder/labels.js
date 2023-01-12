22004c218c003dfa56f8a7f05a62f5b7877c80b0
Cristian <cristianvasquez@gmail.com>
2023-01-12 12:25:00 +0100

    chore: simplify

--- src/builder/labels.js
+++ src/builder/labels.js
@@ -1,20 +1,8 @@
 import { splitIfVocab } from './utils.js'
 
 function getWithLang (cf, options) {
-  for (const lang of options.preferredLanguages) {
-    const terms = cf.out(options.labelProperties, { language: lang }).terms
-    if (terms.length) {
-      const language = terms[0].language
-      return {
-        ...(language && { language }), string: terms[0].value
-      }
-    }
-  }
-  return undefined
-}
-
-function getWithoutLang (cf, options) {
-  const terms = cf.out(options.labelProperties).terms
+  const terms = cf.out(options.labelProperties,
+    { language: [...(options.preferredLanguages ?? []), '*'] }).terms
   if (terms.length) {
     const language = terms[0].language
     return {
@@ -29,29 +17,31 @@ function getLabel (cf, options) {
 
   if (term.termType === 'Literal') {
     const language = term.language
-    const datatype = term.datatype ? splitIfVocab(term.datatype.value) : undefined
+    const datatype = term.datatype
+      ? splitIfVocab(term.datatype.value)
+      : undefined
     return {
-      ...(language && { language }), ...(datatype && { datatype }), string: term.value
+      ...(language && { language }),
+      ...(datatype && { datatype }),
+      string: term.value
     }
   }
 
-  // Get the first label with language tag, using preferredLanguages
-  const datasetLabelLang = options.preferredLanguages ? getWithLang(cf, options) : undefined
-  if (datasetLabelLang) {
-    return datasetLabelLang
-  }
-
-  const datasetLabel = getWithoutLang(cf, options)
+  const datasetLabel = getWithLang(cf, options)
   if (datasetLabel) {
     return datasetLabel
   }
 
   if (options.externalLabels) {
-    const externalLabelLang = options.preferredLanguages ? getWithLang(options.externalLabels.node(term), options) : undefined
+    const externalLabelLang = options.preferredLanguages
+      ? getWithLang(
+        options.externalLabels.node(term), options)
+      : undefined
     if (externalLabelLang) {
       return externalLabelLang
     }
-    const externalLabel = getWithoutLang(options.externalLabels.node(term), options)
+    const externalLabel = getWithLang(options.externalLabels.node(term),
+      options)
     if (externalLabel) {
       return externalLabel
     }
